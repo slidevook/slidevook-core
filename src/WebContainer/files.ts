7834355e9e5939f6f329a5336e113bf95c672a4f
yukimura1227 <takamura1227@gmail.com>
2023-03-21 21:15:46 +0900

    feat: set default style

--- src/WebContainer/files.ts
+++ src/WebContainer/files.ts
@@ -2,6 +2,10 @@ import fs from 'fs';
 
 import { FileSystemTree } from "@webcontainer/api";
 const packageJson             = fs.readFileSync('./files/package.json?raw', 'utf-8');
+
+const stylesIndexTs             = fs.readFileSync('./files/styles/index.ts?raw', 'utf-8');
+const stylesSlidevookDefaultCss = fs.readFileSync('./files/styles/slidevookDefault.css?raw', 'utf-8');
+
 const slidesMarkdown          = fs.readFileSync('./files/slides.md?raw', 'utf-8');
 const multipleEntriesMarkdown = fs.readFileSync('./files/pages/multiple-entries.md?raw', 'utf-8');
 const titleAndConfigMarkdown  = fs.readFileSync('./files/pages/title-and-config.md?raw', 'utf-8');
@@ -19,6 +23,20 @@ export const files:FileSystemTree = {
   },
   public: {
     directory: {
+    }
+  },
+  styles: {
+    directory: {
+      'index.ts': {
+        file: {
+          contents: stylesIndexTs
+        }
+      },
+      'slidevookDefault.css': {
+        file: {
+          contents: stylesSlidevookDefaultCss
+        }
+      },
     }
   },
   pages: {
